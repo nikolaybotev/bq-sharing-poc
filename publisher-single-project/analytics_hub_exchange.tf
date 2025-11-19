3d59a1731316b8d6dc1eac04de38005e5fe0d2c2
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-11-19 14:22:23 -0800

    Qualify single project data exchange and listing names.

--- publisher-single-project/analytics_hub_exchange.tf
+++ publisher-single-project/analytics_hub_exchange.tf
@@ -3,8 +3,8 @@ resource "google_bigquery_analytics_hub_data_exchange" "data_exchange" {
   project          = google_project.bq_publisher.project_id
   location         = var.region
   data_exchange_id = "data_exchange"
-  display_name     = "Data Exchange"
-  description      = "BigQuery data sharing exchange"
+  display_name     = "Data Exchange - Single Project"
+  description      = "BigQuery data sharing exchange (Single Project)"
 
   depends_on = [
     google_project_service.analyticshub_publisher
--- publisher-single-project/analytics_hub_listing.tf
+++ publisher-single-project/analytics_hub_listing.tf
@@ -4,19 +4,19 @@ resource "google_bigquery_analytics_hub_listing" "data_listing" {
   location         = var.region
   data_exchange_id = google_bigquery_analytics_hub_data_exchange.data_exchange.data_exchange_id
   listing_id       = "data_listing"
-  display_name     = "Shared Dataset Listing"
-  description      = "Shared BigQuery dataset"
+  display_name     = "Shared Dataset Listing - Single Project"
+  description      = "Shared BigQuery dataset (Single Project)"
 
   bigquery_dataset {
     dataset = google_bigquery_dataset.shared_dataset.id
   }
 
   publisher {
-    name = "Data Provider"
+    name = "Data Provider - Single Project"
   }
 
   data_provider {
-    name            = "Data Provider"
+    name            = "Data Provider - Single Project"
     primary_contact = var.publisher_primary_contact
   }
 
